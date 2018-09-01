b89b0d9281938f7e5d9a7bb8826e861ed5deaaab
Daniel Ran <danielr1735@gmail.com>
2018-09-01 16:48:39 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,18 @@
 // your Bomb code here!
+
+import React, { Component } from 'react'// your ImageSlider code here!
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { secondsLeft: props.initialCount }
+  }
+
+  render() {
+    if (this.state.secondsLeft !== 0) {
+      return (<div>{this.state.secondsLeft} seconds left before I go boom!</div>)
+    }
+
+    return (<div>Boom!</div>)
+  }
+}
